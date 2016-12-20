a08abb811f30e72a096ffcdabb49bd1fa981d6d4
Ines Ferreira <inesferreira789@gmail.com>
2016-12-20 18:12:17 +0000

    Refactor functions

--- Proj2/board.pl
+++ Proj2/board.pl
@@ -1,22 +1,59 @@
 :-use_module(library(lists)).
 :-use_module(library(clpfd)).
 
-solve(L1, L2, L3, L4, L5, L6, L7):-
-  L1 = [P1,P2,P2,P3,P3,P3,P3],
-  L2 = [P1,P1,P2,P7,P7,P3,P8],
-  L3 = [P4,P5,P2,P2,P7,P8,P8],
-  L4 = [P4,P5,P5,P2,P9,P9,P8],
-  L5 = [P4,P4,P6,P2,P2,P9,P8],
-  L6 = [P4,P11,P6,P6,P2,P10,P10],
-  L7 = [P11,P11,P11,P11,P2,P2,P1],
-  domain(L1,0,1),
-  domain(L2,0,1),
-  domain(L3,0,1),
-  domain(L4,0,1),
-  domain(L5,0,1),
-  domain(L6,0,1),
-  domain(L7,0,1),
-  P1 + P2 + P2 + P3 + P3 + P3 + P3 #= 5,
-  P1 + P1 + P2 + P7 + P7 + P3 + P8 #= 5,
-  P4 + P5 + P5 + P2 + P9 + P9 + P8 #= 4,
-  P4 + P11 + P6 + P6 + P2 + P10 + P10 #= 4.
+
+board_1([
+          [P1,P2,P2,P3,P3,P3,P3],
+          [P1,P1,P2,P7,P7,P3,P8],
+          [P4,P5,P2,P2,P7,P8,P8],
+          [P4,P5,P5,P2,P9,P9,P8],
+          [P4,P4,P6,P2,P2,P9,P8],
+          [P4,P11,P6,P6,P2,P10,P10],
+          [P11,P11,P11,P11,P2,P2,P10]
+          ]).
+
+vertical([5,5,-1,4,-1,4,-1]).
+
+fill_board(_,_,0).
+
+fill_board(Board,Vertical,N):-
+  element(N,Vertical,LineSum),
+  LineSum #= -1,
+  N1 is N - 1,
+  fill_board(Board,Vertical,N1).
+
+
+fill_board(Board,Vertical,N):-
+  select_list(Board, 1, N, List),
+  element(N, Vertical, LineSum),
+  domain(List,0,1),
+  sum(List,#=,LineSum),
+  N1 is N - 1,
+  fill_board(Board,Vertical,N1).
+
+select_list([Head|Tail], N1, N, List):-
+  N1 #\= N,
+  N2 is N1 + 1,
+  select_list(Tail, N2, N, List).
+
+select_list([Head|_], N, N, Head).
+
+select_list([],_,_,[]).
+
+flatten([],[]).
+flatten([LH|LT], Flattened) :-
+	is_list(LH),
+	flatten(LH, FlattenedTemp),
+	append(FlattenedTemp, LT2, Flattened),
+	flatten(LT, LT2).
+
+flatten([LH | LT], [LH | FlattenedT]) :-
+	\+is_list(LH),
+	flatten(LT, FlattenedT).
+
+solve(Board,Vertical,Length):-
+  length(Board,Length),
+  fill_board(Board,Vertical,Length),
+  flatten(Board,BoardFlat),
+  labeling([],BoardFlat),
+  write(BoardFlat).
