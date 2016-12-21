3ee2dc5df072c3f76b120a68fdc162dd2043d902
Ines Ferreira <inesferreira789@gmail.com>
2016-12-21 18:33:59 +0000

    Board 10x10

--- Proj2/board.pl
+++ Proj2/board.pl
@@ -12,8 +12,29 @@ board_1([
           [P11,P11,P11,P11,P2,P2,P10]
           ]).
 
-vertical([5,5,-1,4,-1,4,-1]).
-horizontal([-1,3,-1,-1,-1,5,-1]).
+
+board_2([
+        [P1,P1,P1,P1,P2,P2,P2,P3,P3,P4],
+        [P5,P5,P1,P1,P2,P2,P2,P3,P3,P4],
+        [P6,P7,P7,P8,P8,P8,P3,P3,P3,P4],
+        [P6,P7,P9,P9,P9,P8,P8,P10,P11,P4],
+        [P7,P7,P9,P9,P12,P12,P8,P10,P11,P13],
+        [P7,P7,P16,P16,P12,P14,P15,P15,P13,P13],
+        [P15,P15,P16,P16,P16,P14,P17,P17,P17,P17],
+        [P15,P15,P16,P18,P14,P14,P19,P17,P17,P17],
+        [P20,P20,P16,P18,P14,P14,P19,P21,P21,P22],
+        [P20,P20,P20,P18,P23,P23,P23,P23,P23,P22]
+        ]).
+
+
+
+vertical_1([5,5,-1,4,-1,4,-1]).
+horizontal_1([-1,3,-1,-1,-1,5,-1]).
+
+vertical_2([-1,-1,-1,4,4,3,-1,-1,-1,8]).
+horizontal_2([7,-1,-1,5,-1,-1,5,6,-1,-1]).
+
+
 
 fill_board(_,_,0).
 
@@ -77,10 +98,11 @@ flatten([LH | LT], [LH | FlattenedT]) :-
 	\+is_list(LH),
 	flatten(LT, FlattenedT).
 
-solve(Board,Vertical,Horizontal,Length):-
+solve(Board,Vertical,Horizontal):-
   length(Board,Length),
-  fill_board(Board,Vertical,Length),
   board_vertical(Board, Horizontal,Length),
+  fill_board(Board,Vertical,Length),
+
   flatten(Board,BoardFlat),
   labeling([],BoardFlat),
   write(BoardFlat).
